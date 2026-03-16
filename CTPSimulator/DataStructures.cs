bb181e33bb8e04e4cd0f94f07c20e8df8090343d
vocux <github@sebastian-gruenewald.de>
2026-03-16 05:24:01 +0100

    Slot distributer V1

---
+++ CTPSimulator/DataStructures.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace CTPSimulator
+{
+
+    public class VATSIMEvent
+    {
+        // values coming from the database
+        public string Title { get; set; } // for example CTP 26E
+
+        public uint RouteRevision { get; set; }
+        public uint SlotRevision { get; set; }
+        public DateOnly SynchronizationDate { get; set; }
+        public double SynchronizationLongitude { get; set; } = -30;
+        public TimeSpan DepartureTimeWindow { get; set; } = TimeSpan.FromHours(3); // how long will departure airports depart for?
+
+        public List<Location> Waypoints { get; set; } = new();
+        public List<Airport> Airports { get; set; } = new();
+        public List<RouteSegment> RouteSegments { get; set; } = new();
+
+        [NotMapped]
+        public List<Airport> DepartureAirports { get; set; } = new();
+
+        [NotMapped]
+        public List<Airport> ArrivalAirports { get; set; } = new();
+
+
+        // values populated by the simulator
+        public List<Slot> Slots { get; set; } = new();
+        public TimeSpan SimulationAnalysisResolution { get; set; } = TimeSpan.FromMinutes(2);
+
+
+        // values / functions only for the simulator internally
+        public ushort ConvertToMaximumSlots(ushort maximumAircraftPerHours) => (ushort)(maximumAircraftPerHours * DepartureTimeWindow.TotalHours);
+    }
+
+    public enum RouteSegmentType { AMAS, EMEA, NAT }
+
+
+    public abstract class ThroughputPoint
+    {
+        // values coming from the database
+        public string Identifier { get; set; } // for example SPESA or EDDF or "PORTI_BOS_1", or oceanic track "M"
+
+        public ushort MaximumAircraftPerHour { get; set; } = 20; // default for waypoints and route segments, airports will override this
+
+        // values populated by the simulator
+        public ushort SlotsAllocated { get; set; }
+
+
+        // values / functions only for the simulator internally
+        public ushort MaximumSlots { get; set; }
+
+        [NotMapped]
+        public int SlotsStillAvailable => MaximumSlots - SlotsAllocated;
+
+        [NotMapped]
+        public bool AreSlotsStillAvailable => SlotsAllocated < MaximumSlots;
+    }
+
+    public class Location : ThroughputPoint // for waypoint or airport
+    {
+        // values coming from the database
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        // values populated by the simulator
+        public DateTime SimulationAnalysisStartTime { get; set; } // for example 12z at the given date with graphical resolution of 10 minutes...
+        public List<Slot> SlotsAnalysisFrames { get; set; } // if first element is at 12z, second at 1202z, third at 1204z, etc...
+
+        // values / functions only for the simulator internally
+    }
+
+    public class Airport : Location
+    {
+        // values populated by the simulator
+        public DateTime DepartureTimeWindowStart { get; set; }
+
+        public ushort NumberOfVotes { get; set; }
+
+        [NotMapped]
+        public List<RouteSegment> ConnectingPrimaryRouteSegments { get; set; } = new ();
+
+        [NotMapped]
+        public List<RouteSegment> ConnectingSecondaryRouteSegments { get; set; } = new();
+    }
+
+    public class RouteSegment : ThroughputPoint
+    {
+        // values coming from the database
+        public string RouteString { get; set; } // for example "MARUN Y150 TOLGI SAS P605 NOLGO" or "RESNO 5520N 5530N 5540N 5550N LOMSI"
+        public RouteSegmentType Type { get; set; }
+        public List<Location> Locations { get; set; } = new(); // can be waypoints or airports
+    }
+
+    public class Slot
+    {
+        // values populated by the simulator
+        public List<RouteSegment> RouteSegments { get; set; } = new();
+
+        public DateTime DepartureTime { get; set; }
+        public DateTime ProjectedArrivalTime { get; set; }
+
+        public Airport DepartureAirport { get; set; }
+        public Airport ArrivalAirport { get; set; }
+
+
+        // a bunch of values must be stored that are outside the scope of my simulator, like
+        // CID:
+        // Aircraft Type:
+        // ...
+    }
+}
---
+++ CTPSimulator/SlotDistributionCreator.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace CTPSimulator
+{
+    public static class SlotDistributionCreator
+    {
+        static readonly Random Random = new Random();
+        static T PickOne<T>(this List<T> input) => input[Random.Next(input.Count)];
+
+        public static async Task CreateSlotDistribution(VATSIMEvent vatsimEvent, CancellationToken cancellationToken = default)
+        {
+            // prepare data
+            vatsimEvent.DepartureAirports = vatsimEvent.Airports.Where(a => vatsimEvent.RouteSegments.Exists(r => r.Locations.First() == a)).ToList();
+            vatsimEvent.ArrivalAirports = vatsimEvent.Airports.Where(a => vatsimEvent.RouteSegments.Exists(r => r.Locations.Last() == a)).ToList();
+
+            foreach (var airport in vatsimEvent.DepartureAirports)
+            {
+                airport.ConnectingPrimaryRouteSegments = vatsimEvent.RouteSegments.Where(r => r.Type != RouteSegmentType.NAT &&
+                    r.Locations.First() == airport).ToList();
+
+                airport.ConnectingSecondaryRouteSegments = vatsimEvent.RouteSegments.Where(r => r.Type == RouteSegmentType.NAT &&
+                    airport.ConnectingPrimaryRouteSegments.Exists(pr => pr.Locations.Last() == r.Locations.First())).ToList();
+            }
+            foreach (var airport in vatsimEvent.ArrivalAirports)
+            {
+                airport.ConnectingPrimaryRouteSegments = vatsimEvent.RouteSegments.Where(r => r.Type != RouteSegmentType.NAT &&
+                    r.Locations.Last() == airport).ToList();
+
+                airport.ConnectingSecondaryRouteSegments = vatsimEvent.RouteSegments.Where(r => r.Type == RouteSegmentType.NAT &&
+                    airport.ConnectingPrimaryRouteSegments.Exists(tr => tr.Locations.First() == r.Locations.Last())).ToList();
+            }
+
+            while (true)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+
+                // take all possible options where slots could go
+                List<(Airport, RouteSegment, Airport, int, int, int)> choices = new();
+                foreach (var departureAirport in vatsimEvent.DepartureAirports.Where(da => da.AreSlotsStillAvailable))
+                {
+                    var possibleRouteSegments = departureAirport.ConnectingSecondaryRouteSegments.Where(sr => sr.AreSlotsStillAvailable).ToList();
+                    foreach (var routeSegment in possibleRouteSegments)
+                    {
+                        var possibleArrivals = vatsimEvent.ArrivalAirports.Where(aa => aa.AreSlotsStillAvailable && aa.ConnectingSecondaryRouteSegments.Contains(routeSegment)).ToList();
+                        foreach (var arrivalAirport in possibleArrivals)
+                        {
+                            choices.Add((departureAirport, routeSegment, arrivalAirport,
+                                possibleRouteSegments.Count + possibleArrivals.Count,
+                                departureAirport.SlotsStillAvailable + routeSegment.SlotsStillAvailable + arrivalAirport.SlotsStillAvailable,
+                                departureAirport.NumberOfVotes + arrivalAirport.NumberOfVotes));
+                        }
+                    }
+                }
+
+                if (choices.Count == 0) break;
+
+                var choice = choices.OrderBy(c => c.Item4).ThenByDescending(c => c.Item5).ThenByDescending(c => c.Item6).First();
+                choice.Item1.SlotsAllocated++;
+                choice.Item2.SlotsAllocated++;
+                choice.Item3.SlotsAllocated++;
+
+                var firstRouteSegment = choice.Item1.ConnectingPrimaryRouteSegments.Where(pr => pr.Locations.Last() == choice.Item2.Locations.First()).OrderBy(pr => pr.SlotsAllocated).First();
+                firstRouteSegment.SlotsAllocated++;
+
+                var thirdRouteSegment = choice.Item3.ConnectingPrimaryRouteSegments.Where(pr => pr.Locations.First() == choice.Item2.Locations.Last()).OrderBy(pr => pr.SlotsAllocated).First();
+                thirdRouteSegment.SlotsAllocated++;
+
+                vatsimEvent.Slots.Add(new Slot()
+                {
+                    DepartureAirport = choice.Item1,
+                    RouteSegments = new()
+                    {
+                        firstRouteSegment, // airport to nat
+                        choice.Item2, // nat track
+                        thirdRouteSegment // nat to airport
+                    },
+                    ArrivalAirport = choice.Item3
+                });
+            }
+        }
+    }
+}
